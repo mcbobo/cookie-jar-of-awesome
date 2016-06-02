c763fee79c67ac225c413d79ee81bf6a0e927e70
Bo Li <bo@lendingloop.ca>
2016-06-02 18:53:34 -0400

    Working save and sample

--- db/Awesome/index.js
+++ db/Awesome/index.js
@@ -5,8 +5,27 @@ import config from '../../config';
 export function storeAwesome(awesome) {
   r.connect(config.rethinkdb)
     .then(conn => {
-      r.table('awesome').insert(awesome)
-        .then(res => return res)
-        .catch(err => return err);
+      r.table('awesome').insert(awesome).run(conn)
+        .then(res => {
+          return res;
+        })
+        .catch(err => {
+          return err;
+        });
     });
+}
+
+export function getAwesome(num) {
+  return new Promise((resolve, reject) => {
+    r.connect(config.rethinkdb)
+      .then(conn => {
+        r.table('awesome').sample(num).run(conn)
+          .then(res => {
+            return resolve(res);
+          })
+          .catch(err => {
+            return reject(err);
+          });
+      });
+  });
 }
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 import koaRouter from 'koa-router';
-import { storeAwesome } from '../db/Awesome';
+import { storeAwesome, getAwesome } from '../db/Awesome';
 
 const router = koaRouter();
 
 router.post('/awesome', function * (next) {
-  console.log(this.request.body);
-  const awesome = this.request.body.awesome;
+  const awesome = this.request.body;
 
   this.body = storeAwesome(awesome);
+});
+
+router.get('/awesome', function * (next) {
+  this.body = yield getAwesome(10);
 });
 
 router.get('/', function * (next) {
